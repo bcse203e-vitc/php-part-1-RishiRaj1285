209b4f9983175efac107378e693b811305fb5890
Rishi Raj <pothanaboyina.rishi2023@vitstudent.ac.in>
2025-09-22 00:29:25 +0530

    Create Q4.php

---
+++ Assignment 8/Q4.php
@@ -0,0 +1,13 @@
+<?php
+$x = array(1, 2, 3, 4, 5);
+
+echo "Original Array:\n";
+var_dump($x);
+
+unset($x[3]);
+
+$x = array_values($x);
+
+echo "<br>Array After Deletion and Normalization:\n";
+var_dump($x);
+?>
