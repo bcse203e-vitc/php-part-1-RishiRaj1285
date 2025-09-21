64b0437c56b8bbbf59f31a5e628c1988e6c85d10
Rishi Raj <pothanaboyina.rishi2023@vitstudent.ac.in>
2025-09-22 00:31:24 +0530

    Create Q8.php

---
+++ Assignment 8/Q8.php
@@ -0,0 +1,11 @@
+<?php
+    $a=array("Sophia"=>"31","Jacob"=>"41","William"=>"39","Ramesh"=>"40");
+    sort($a);
+    print_r($a);
+    rsort($a);
+    print_r($a);
+    ksort($a);
+    print_r($a);
+    krsort($a);
+    print_r($a);
+?>
