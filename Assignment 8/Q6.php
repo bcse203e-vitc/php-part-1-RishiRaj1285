56689f9435edee7ffe68f729aeba3a6421fd5fe8
Rishi Raj <pothanaboyina.rishi2023@vitstudent.ac.in>
2025-09-22 00:30:27 +0530

    Create Q6.php

---
+++ Assignment 8/Q6.php
@@ -0,0 +1,14 @@
+<?php
+    $filePath='sample.json';
+    $jsonData=file_get_contents($filePath);
+    if($jsonData==false){
+        echo "Failed to fetch json";
+    }
+    else{
+        $data=json_decode($jsonData);
+        foreach($data as $k=>$v){
+            echo "{$k} : {$v}<br>";
+        }
+        echo "Publisher : {$data->Detail->Publisher}<br>";
+    }
+?>
